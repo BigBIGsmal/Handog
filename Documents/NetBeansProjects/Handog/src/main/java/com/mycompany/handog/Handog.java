e4f3e4fe1892547b57a43f0e82abcae8c436af3a
Rael <rafaeldronquillo@gmail.com>
2023-05-06 12:28:39 +0800

    haha

---
+++ Documents/NetBeansProjects/Handog/src/main/java/com/mycompany/handog/Handog.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.handog;
+
+/**
+ *
+ * @author admin
+ */
+public class Handog {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
