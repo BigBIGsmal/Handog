db49942714c5a5967c0c4f7ae1dce6621adf8077
Rael <rafaeldronquillo@gmail.com>
2023-05-11 01:50:15 +0800

    medyo okay na gagawin ko na ung queue

--- Documents/NetBeansProjects/Handog/src/model/RequestsModel.java
+++ Documents/NetBeansProjects/Handog/src/model/RequestsModel.java
@@ -6,6 +6,7 @@ package model;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Queue;
 import model.Request;
 
@@ -17,7 +18,7 @@ public class RequestsModel {
         
     private String brgy="",city="", category="", email="", contactNum="", duration="", status="";
     private LocalDate date;
-    private Queue<Request> request = new Queue<>();
+    private Queue<Request> request = new LinkedList<Request>();
 
     
     
